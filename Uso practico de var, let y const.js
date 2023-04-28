238323a7e0c5b33d5391b2eb2e656f635037f530
Juan Diego Mejia <mejiavilladiego@hotmail.com>
2023-04-28 17:50:56 -0500

    Tipos de datos, variables

---
+++ Uso practico de var, let y const.js
@@ -0,0 +1,9 @@
+// Uso practico de var, let y const 
+nombre = 'juan'; // es posible crear la variable sin la palabra reservada, pero no es buena practica
+console.log(nombre);
+
+
+// No se pueden modificar 
+const apellido = 'Perez';
+apellido = 'Lara';
+
